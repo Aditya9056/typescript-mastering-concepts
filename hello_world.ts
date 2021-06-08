acb55ae3682f5caf9300866801fa66145457c56d
Aditya Jain <vvasujain@gmail.com>
2021-06-08 23:33:09 +0530

    variables & objects

--- hello_world.ts
+++
@@ -1,5 +0,0 @@
-function greeter(person: number){
-    return "Hello "+person;
-}
-
-console.log(greeter(343));
---
+++ src/variables.ts
@@ -0,0 +1,10 @@
+// between 500 and 1000
+const RANDOM_WAIT_TIME =
+  Math.round(Math.random() * 500) + 500
+let startTime = new Date()
+let endTime: any
+
+setTimeout(() => {
+  endTime = 0
+  endTime = new Date()
+}, RANDOM_WAIT_TIME)
